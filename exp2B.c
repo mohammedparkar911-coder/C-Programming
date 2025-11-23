9fcdbb06fdac3586c0d987d871457a98c8f2ca4f
mohammedparkar911-coder <mohammedparkar911@gmail.com>
2025-11-23 12:43:13 +0300

    Update exp2B.c

--- exp2B.c
+++ exp2B.c
@@ -2,14 +2,14 @@
 
 int main() {
     int a, b, sum = 0;
-    printf("Enter two numbers \n");
+    printf("Enter two numbers: \n");
     scanf("%d %d", &a, &b);
-    printf("Entered two numbers r: %d %d \n", a, b);
+    printf("Entered two numbers r: %d & %d \n", a, b);
 
     for(int i = a; i <= b; i++)
         if(i % 2 != 0)
             sum += i;
 
-    printf("Sum of 2 nos = %d", sum);
+    printf("Sum of all odd nos between %d & %d  = %d", a, b ,sum);
     return 0;
 }
